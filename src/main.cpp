df74474f74ca28a8457c1cefde06cf222eab6265
MisakiAkeno <cxf213@outlook.com>
2023-04-04 12:31:39 +0800

    basic test of LED and serial port

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,15 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Hello world!");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
