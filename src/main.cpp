668a322d8269d8fe05bdc9a77ee62048f7ca2666
MisakiAkeno <cxf213@outlook.com>
2023-04-05 19:59:59 +0800

    WebServer Opti

--- src/main.cpp
+++ src/main.cpp
@@ -32,47 +32,28 @@ const int LED2 = 15;
 
 bool led1State = false;
 bool led2State = false;
-
+String HTMLstr;
 void sendHtml() {
-  String response = R"(
-    <!DOCTYPE html><html>
-      <head>
-        <title>ESP32 Web Server Demo</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-          html { font-family: sans-serif; text-align: center; }
-          body { display: inline-flex; flex-direction: column; }
-          h1 { margin-bottom: 1.2em; } 
-          h2 { margin: 0; }
-          div { display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: auto auto; grid-auto-flow: column; grid-gap: 1em; }
-          .btn { background-color: #5B5; border: none; color: #fff; padding: 0.5em 1em;
-                 font-size: 2em; text-decoration: none }
-          .btn.OFF { background-color: #333; }
-        </style>
-      </head>
-            
-      <body>
-        <h1>ESP32 Web Server</h1>
-        <div>
-          <h2>LED 1</h2>
-          <a href="/toggle/1" class="btn LED1_TEXT">LED1_TEXT</a>
-          <h2>LED 2</h2>
-          <a href="/toggle/2" class="btn LED2_TEXT">LED2_TEXT</a>
-        </div>
-      </body>
-    </html>
-  )";
+  String response = String(HTMLstr);
   response.replace("LED1_TEXT", led1State ? "ON" : "OFF");
   response.replace("LED2_TEXT", led2State ? "ON" : "OFF");
   server.send(200, "text/html", response);
+}
+
+void putText(String s,int xpos=10,int ypos=10,int size=1){
+  display.setTextSize(size);         // set text size
+  display.setTextColor(WHITE);
+  display.setCursor(xpos,ypos);
+  display.print(s);
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+  //pinMode(GPIO_NUM_1, INPUT);
 
-  //Wire.begin();
+  // 显示初始化
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     while (true);
@@ -81,93 +62,62 @@ void setup() {
   delay(200);
   display.clearDisplay(); // Clear the buffer
   display.display(); // Display the buffer
-  display.setTextSize(1);         // set text size
-  display.setTextColor(WHITE);
-  display.setCursor(10,10);
-  display.print("Hello.");
+  putText("Hello!",30,22,2);
   display.display();
 
-
+  //WIFI初始化
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD, WIFI_CHANNEL);
   Serial.print("Connecting to WiFi ");
   Serial.print(WIFI_SSID);
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
     Serial.print(".");
   }
-
   Serial.println(" Connected!");
-
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", sendHtml);
+  //SPIFFS存储初始化
+  if(!SPIFFS.begin(true)){Serial.println("An Error has occurred while mounting SPIFFS");return;
+  }
+  File file = SPIFFS.open("/index.html");
+  if(!file){Serial.println("Failed to open file!");return;}
+  while(file.available()){
+    //Serial.write(file.read());
+    HTMLstr.concat(char(file.read()));
+  }
+  file.close();
 
+  //WebSever初始化
+  server.on("/", sendHtml);
   server.on(UriBraces("/toggle/{}"), []() {
     String led = server.pathArg(0);
     Serial.print("Toggle LED #");
     Serial.println(led);
-
     switch (led.toInt()) {
       case 1:
         led1State = !led1State;
+        display.clearDisplay();
+        putText("Toggle LED 1");
+        display.display();
         digitalWrite(LED1, led1State);
         break;
       case 2:
         led2State = !led2State;
+        display.clearDisplay();
+        putText("Toggle LED 2");
+        display.display();
         digitalWrite(LED2, led2State);
         break;
     }
 
     sendHtml();
   });
-
   server.begin();
   Serial.println("HTTP server started (http://localhost:80)");
-  if(!SPIFFS.begin(true)){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-  File file = SPIFFS.open("/test.txt");
-  if(!file){
-    Serial.println("Failed to open file!");
-    return;
-  }
-  
-  Serial.println("Content of file:");
-  while(file.available()){
-    Serial.write(file.read());
-  }
-  file.close();
 }
-
-
-// #include <Arduino.h>
-// void setup() {
-//   // put your setup code here, to run once:
-//   pinMode(LED_BUILTIN, OUTPUT);
-//   Serial.begin(9600);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   digitalWrite(LED_BUILTIN, HIGH);
-//   Serial.println("Hello world!");
-//   delay(100);
-//   digitalWrite(LED_BUILTIN, LOW);
-//   delay(100);
-// }
 int in = 0;
 void loop() {
-  display.clearDisplay();
-
-  display.setTextSize(1);         // set text size
-  display.setTextColor(WHITE);
-  display.setCursor(10,10);
-  display.print("Hello."+in);
-  display.display();
   server.handleClient();
-  //in++;
   delay(100);
 }
